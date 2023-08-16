74aa0281082c9aef32e9a7c78842796250aa1c15
Adinath Shelke <shelkeadinath3@gmail.com>
2023-08-16 20:16:12 +0530

    added image and aligned it

---
+++ components/HeaderSocials.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+const HeaderSocials = () => {
+  const myStyle = {
+    fontSize: "20px",
+  };
+  return (
+    <div className="header__socials flex flex-col gap-[1.2rem] absolute left-0 bottom-[3rem] items-center ">
+      <Link
+        className="link"
+        style={myStyle}
+        href="https://linkedin.com"
+        target="_blank"
+      >
+        <AiFillLinkedin />
+      </Link>
+      <Link
+        className="link"
+        style={myStyle}
+        href="https://github.com"
+        target="_blank"
+      >
+        <BsGithub />
+      </Link>
+      <Link
+        className="link"
+        style={myStyle}
+        href="https://instagram.com"
+        target="_blank"
+      >
+        <BsInstagram />
+      </Link>
+    </div>
+  );
+};
+
+export default HeaderSocials;
