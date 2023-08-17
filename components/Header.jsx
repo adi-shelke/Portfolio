dac10310b5c6f77c8e74d7fb85af6886a48e08f2
Adinath Shelke <shelkeadinath3@gmail.com>
2023-08-17 19:37:20 +0530

    dummy 2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,6 @@
 import "../styles/header.css";
-import Buttons from "./Buttons";
-import Image from "next/image";
-import Link from "next/link";
-import HeaderSocials from "./HeaderSocials";
 const Header = () => {
-  return (
-    <header>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Adinath Shelke</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
-        <Buttons />
-
-        <HeaderSocials />
-
-        <div className="me mt-[4rem]">
-          {/* <Image src="/src/assets/me1_bg.png" height={8000} width={400} /> */}
-          <img src="/src/assets/me1_bg.png" alt="profile" />
-        </div>
-
-        <Link className="scroll__down link" href="#contact">
-          Scroll Down
-        </Link>
-      </div>
-    </header>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
